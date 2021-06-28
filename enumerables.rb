bb6a200bade5c7f55afcf3f992871ecab8192885
Samuel Jeon <samueljeon99@gmail.com>
2021-06-28 16:46:38 -0400

    first Commit

---
+++ enumerables.rb
@@ -0,0 +1,76 @@
+
+class Array
+
+    def my_each(&prc)
+        i=0
+        while i < self.length
+            prc.call(self[i])
+            i+=1
+        end
+        self
+    end
+
+    def my_select(&prc)
+        new_array=[]
+        self.my_each do |ele|
+            new_array << ele if prc.call(ele)
+        end
+        return new_array
+    end
+
+    def my_reject(&prc)
+        new_array=[]
+        self.my_each do |ele|
+            new_array << ele if !prc.call(ele)
+        end
+        new_array
+    end
+
+    def my_any?(&prc)
+        self.my_each do |ele|
+            return true if prc.call(ele)
+        end
+        false
+    end
+
+    def my_all?(&prc)
+        self.my_each do |ele|
+            return false if !prc.call(ele)
+        end
+        true
+    end
+
+    def my_flatten
+        return self if self.length ==1
+        new_array=[]
+        self.each do |ele|
+            new_array += [ele].my_flatten
+        end
+        new_array
+    end
+
+
+end
+
+# return_value = [1, 2, 3].my_each do |num|
+#   puts num
+# end.my_each do |num|
+#   puts num
+# end
+# p return_value  # => [1, 2, 3]
+
+# a = [1, 2, 3]
+# p a.my_select { |num| num > 1 } # => [2, 3]
+# p a.my_select { |num| num == 4 } # => []
+
+# a = [1, 2, 3]
+# p a.my_reject { |num| num > 1 } # => [1]
+# p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+
+# a = [1, 2, 3]
+# p a.my_any? { |num| num > 1 } # => true
+# p a.my_any? { |num| num == 4 } # => false
+# p a.my_all? { |num| num > 1 } # => false
+# p a.my_all? { |num| num < 4 } # => true
+
+p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
