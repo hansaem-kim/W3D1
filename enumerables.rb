a9f207b6b7840a9a47afce0deeb23dc70c2341ad
Hansaem Kim <hansaem_kim@hotmail.com>
2021-06-28 19:17:49 -0400

    Hansaem's First

--- enumerables.rb
+++ enumerables.rb
@@ -49,6 +49,17 @@ class Array
         new_array
     end
 
+    def my_zip(*args)
+        new_array = []
+        self.each.with_index do |ele, i|
+            sub = [ele]
+            args.each do |arg|
+                sub << arg[i]
+            end
+            new_array << sub
+        end
+        new_array
+    end
 
 end
 
@@ -73,4 +84,14 @@ end
 # p a.my_all? { |num| num > 1 } # => false
 # p a.my_all? { |num| num < 4 } # => true
 
-p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+# p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
